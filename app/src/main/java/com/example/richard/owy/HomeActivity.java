21b23d7ef989b6740d13581be159e1636327b3e0
Richard <richard.vd.knaap@outlook.com>
2018-05-18 18:27:14 +0100

    Buttons naar verschillende screens gemaakt

--- app/src/main/java/com/example/richard/owy/HomeActivity.java
+++ app/src/main/java/com/example/richard/owy/HomeActivity.java
@@ -7,14 +7,56 @@ import android.view.View;
 import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-    private Button button;
+
+    public Button but1;
+    public Button but2;
+    public Button but3;
+    public Button but4;
+
+    public  void init(){
+        but2 = (Button)findViewById(R.id.button2);
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy = new Intent(HomeActivity.this,DeptActivity.class);
+                startActivity(toy);
+            }
+        });
+        but1 = (Button)findViewById(R.id.button);
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy = new Intent(HomeActivity.this,ReceiveActivity.class);
+                startActivity(toy);
+            }
+        });
+        but3 = (Button)findViewById(R.id.button3);
+        but3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy = new Intent(HomeActivity.this,InfoReceiveActivity.class);
+                startActivity(toy);
+            }
+        });
+        but4 = (Button)findViewById(R.id.button4);
+        but4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy = new Intent(HomeActivity.this,InfoDeptActivity.class);
+                startActivity(toy);
+            }
+        });
+
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        init();
 
     }
 }
-
-
