c5c40aa523be804c30e62393620bf6e8823e9501
Cerdjee4 <31687157+Cerdjee4@users.noreply.github.com>
2018-06-03 22:45:08 +0200

    Verder gewerkt
    Frontend van Debt en Receive pages zijn gemaakt.  Te doen: Homepage afmaken, info pages maken voor debt en receive, formulieren maken voor debt en receive,

--- app/src/main/java/com/example/richard/owy/DeptActivity.java
+++ app/src/main/java/com/example/richard/owy/DeptActivity.java
@@ -1,13 +1,20 @@
 package com.example.richard.owy;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 
 public class DeptActivity extends AppCompatActivity {
+
+    ListView debtListView;
+    String[] naamSchuld;
+    String[] schuldBedrag;
+
 
     public Button but1;
 
@@ -27,6 +34,15 @@ public class DeptActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dept);
+
+        Resources res = getResources();
+
+        debtListView = (ListView) findViewById(R.id.debtListView);
+        naamSchuld = res.getStringArray(R.array.naamSchuld);
+        schuldBedrag = res.getStringArray(R.array.schuldBedrag);
+        debtPageAdapter debtPageAdapter = new debtPageAdapter(this, naamSchuld, schuldBedrag);
+        debtListView.setAdapter(debtPageAdapter);
+
         //init();
     }
 }
