3a79e5e1665a867ba5773794df8106e5a6ca5cd7
Cerdjee4 <31687157+Cerdjee4@users.noreply.github.com>
2018-06-14 20:25:25 +0200

    meer UI dingen

--- app/src/main/java/com/example/richard/owy/debtInfoPageActivity.java
+++ app/src/main/java/com/example/richard/owy/debtInfoPageActivity.java
@@ -4,21 +4,16 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-import java.io.File;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 
 public class debtInfoPageActivity extends AppCompatActivity {
     String[] naamSchuld;
     String[] schuldBedrag;
+    String[] usernameSchuld;
+    String[] datumSchuld;
+    String[] ibanSchuld;
+    String[] beschrijvingSchuld;
 
 
     @Override
@@ -31,11 +26,26 @@ public class debtInfoPageActivity extends AppCompatActivity {
         int position = mIntent.getIntExtra("varName",1);
         naamSchuld = res.getStringArray(R.array.naamSchuld);
         schuldBedrag = res.getStringArray(R.array.schuldBedrag);
+        usernameSchuld = res.getStringArray(R.array.usernameSchuld);
+        datumSchuld = res.getStringArray(R.array.datumSchuld);
+        ibanSchuld = res.getStringArray(R.array.ibanSchuld);
+        beschrijvingSchuld = res.getStringArray(R.array.beschrijvingSchuld);
+
         TextView tv = (TextView)findViewById(R.id.bedragDebtTextView);
         TextView tv2 = (TextView)findViewById(R.id.showNaamTextView);
+        TextView tv3 = (TextView)findViewById(R.id.debtUserTextView);
+        TextView tv4 = (TextView)findViewById(R.id.DatumDebtTextView);
+        TextView tv5 = (TextView)findViewById(R.id.ibandebtTextView);
+        TextView tv6 = (TextView)findViewById(R.id.beschrijvingTextView);
 
-        tv2.setText(naamSchuld[position]);
         tv.setText(schuldBedrag[position]);
+        tv2.setText(naamSchuld[position]);
+        tv3.setText(usernameSchuld[position]);
+        tv4.setText(datumSchuld[position]);
+        tv5.setText(ibanSchuld[position]);
+        tv6.setText(beschrijvingSchuld[position]);
+
+        tv6.setMovementMethod(new ScrollingMovementMethod());
 
     }
 }
